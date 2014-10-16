bfe1d9f0999480738abeee8b4129e817038d1e8f
Darko Grozdanovski <darkogrozdanovski@gmail.com>
2014-10-16 16:28:51 +0200

    base classes

--- app/src/main/java/com/aranea_apps/android/libs/commons/sample/MainActivity.java
+++ app/src/main/java/com/aranea_apps/android/libs/commons/sample/MainActivity.java
@@ -3,13 +3,22 @@ package com.aranea_apps.android.libs.commons.sample;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
+import com.aranea_apps.android.libs.commons.notifications.BaseNotificationUtil;
+
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    BaseNotificationUtil.makeSingleShowToast("Hello " + PreferenceUtil.getRememberMePreference().get());
+
+
+    PreferenceUtil.getRememberMePreference().set(!PreferenceUtil.getRememberMePreference().get());
+
+
+  }
 
 }
