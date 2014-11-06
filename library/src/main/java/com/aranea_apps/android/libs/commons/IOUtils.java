96137812340538bd622542508c1f1d4295c9a3fa
Darko Grozdanovski <darkogrozdanovski@gmail.com>
2014-11-06 16:42:07 +0100

    removed logger, added IO Utils

---
+++ library/src/main/java/com/aranea_apps/android/libs/commons/IOUtils.java
@@ -0,0 +1,33 @@
+package com.aranea_apps.android.libs.commons;
+
+public class IOUtils {
+
+  public static String toString(InputStream input) throws IOException {
+    StringWriter sw = new StringWriter();
+    InputStreamReader in = new InputStreamReader(input);
+    copy(in, sw);
+    return sw.toString();
+  }
+
+  public static int copy(Reader input, Writer output) throws IOException {
+    long count = copyLarge(input, output);
+    if (count > Integer.MAX_VALUE) {
+      return -1;
+    }
+    return (int) count;
+  }
+
+  private static final int DEFAULT_BUFFER_SIZE = 1024 * 4;
+
+  public static long copyLarge(Reader input, Writer output) throws IOException {
+    char[] buffer = new char[DEFAULT_BUFFER_SIZE];
+    long count = 0;
+    int n = 0;
+    while (-1 != (n = input.read(buffer))) {
+      output.write(buffer, 0, n);
+      count += n;
+    }
+    return count;
+  }
+
+}
